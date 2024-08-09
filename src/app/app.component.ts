4985aaec7643d3889a1ce12c345d5ff4e77a5b1f
Filip Uskovic <filip.uskovic@outlook.com>
2024-08-09 09:34:00 +0200

    dodao sam bootstrap i font awesome, takoder dodao proxy conf.file  te inicijalizirao fontawesome za sada. Promjenio selectro iz app u wac

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 
 @Component({
-  selector: 'app-root',
+  selector: 'wac-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'whatsup-clone-front';
+
+  // incijaliziram fontawesome liabry
+  private faIconLibrary = inject(FaIconLibrary)
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons)
+  }
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,32 @@
+import {IconDefinition} from '@fortawesome/free-regular-svg-icons';
+import {
+  faAdd,
+  faArrowLeft,
+  faCheck,
+  faEllipsisVertical,
+  faFaceLaugh,
+  faFileImport,
+  faHeadphones,
+  faImage,
+  faPaperPlane,
+  faSearch,
+  faUser,
+  faVideo
+} from '@fortawesome/free-solid-svg-icons';
+
+// ovo radim jer ne zelim imporatati sve fontawseome icons- bundle bi bio pre velik
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faEllipsisVertical,
+  faUser,
+  faArrowLeft,
+  faSearch,
+  faAdd,
+  faFaceLaugh,
+  faPaperPlane,
+  faFileImport,
+  faImage,
+  faHeadphones,
+  faVideo,
+  faCheck
+]
